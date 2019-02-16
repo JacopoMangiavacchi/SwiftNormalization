da18b2ce22a757d3eab31c61c10f6e2cf9e9492a
Jacopo Mangiavacchi <jamangia@microsoft.com>
2019-02-16 13:56:36 -0800

    Added Max

--- Sources/SwiftNormalization/SwiftNormalization.swift
+++ Sources/SwiftNormalization/SwiftNormalization.swift
@@ -13,3 +13,17 @@ public func minMax<T: FloatingPoint>(_ array : [T]) -> [T] {
     guard let (min, max) = minimumMaximum(array) else { return array }
     return array.map{ ($0 - min) / (max - min) }
 }
+
+/**
+ Max Normalization.
+
+ Formula: x' = x' / max
+ Normalized values: 0 <= x' <= 1
+
+ - Parameter array: The floating point array to be normalized.
+ - Returns: The normalized array.
+ */
+public func max<T: FloatingPoint>(_ array : [T]) -> [T] {
+    guard let (_, max) = minimumMaximum(array) else { return array }
+    return array.map{ $0 / max }
+}
--- Tests/SwiftNormalizationTests/SwiftNormalizationTests.swift
+++ Tests/SwiftNormalizationTests/SwiftNormalizationTests.swift
@@ -18,8 +18,26 @@ final class SwiftNormalizationTests: XCTestCase {
         XCTAssertEqual(normalizedArray, expectedNormalizedArray)
     }
 
+    func testMaxFloat() {
+        let array: [Float] = [1,2,3,4,5]
+        let expectedNormalizedArray: [Float] = [0.2, 0.4, 0.6, 0.8, 1.0]
+        let normalizedArray = max(array)
+
+        XCTAssertEqual(normalizedArray, expectedNormalizedArray)
+    }
+
+    func testMaxDouble() {
+        let array: [Double] = [1,2,3,4,5]
+        let expectedNormalizedArray: [Double] = [0.2, 0.4, 0.6, 0.8, 1.0]
+        let normalizedArray = max(array)
+
+        XCTAssertEqual(normalizedArray, expectedNormalizedArray)
+    }
+
     static var allTests = [
         ("testMinMax", testMinMaxFloat),
         ("testMinMax", testMinMaxDouble),
+        ("testMax", testMaxFloat),
+        ("testMax", testMaxDouble),
     ]
 }
